39e5a8fdb6c541aa73a009d9631fc7000c73bd78
jack421537 <irisjalle@gmail.com>
2025-11-06 12:34:02 +0300

    Add files via upload

---
+++ CAT2.QA_part(i and ii).c
@@ -0,0 +1,10 @@
+An array is a collection of elements of the same data type,stored in contiguous memory locations and accessed using a common name with an index
+int scores1[2][2] = {
+    {65, 92},
+    {84, 72}
+};
+
+int scores2[2][2] = {
+    {35, 70},
+    {59, 67}
+};
